ca0231f11ffc1675029e04f776c2d836fdb2feda
Minhkz <minhluky0@gmail.com>
2025-10-30 14:56:47 +0700

    'csdl'

--- data.sql
+++ data.sql
@@ -18,6 +18,9 @@
 --
 -- Table structure for table `brands`
 --
+DROP DATABASE IF EXISTS cellworld_db;
+CREATE DATABASE cellworld_db;
+USE cellworld_db;
 
 DROP TABLE IF EXISTS `brands`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
