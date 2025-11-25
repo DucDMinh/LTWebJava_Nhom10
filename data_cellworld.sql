fceb54cf4e4ff8d1f92c0dc1b8b341ed9ed4bc3c
DoManhTruongg205 <domanhtruong2k5@gmail.com>
2025-11-25 21:23:16 +0700

    file sql

--- data_cellworld.sql
+++ data_cellworld.sql
@@ -12,7 +12,7 @@ CREATE TABLE users(
 	id 				INT AUTO_INCREMENT PRIMARY KEY,
     username 		VARCHAR(100) UNIQUE,
 	email			VARCHAR(100) NOT NULL UNIQUE,
-    `password`		VARCHAR(50),
+    `password`		VARCHAR(100),
     full_name		NVARCHAR(100)  NOT NULL,
     address			NVARCHAR(100),
     phone			CHAR(15) UNIQUE,
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS pro_configuration (
 CREATE TABLE IF NOT EXISTS carts (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     sum INT DEFAULT 0,
-    user_id BIGINT,
+    user_id INT,
     CONSTRAINT fk_carts_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -64,3 +64,30 @@ CREATE TABLE IF NOT EXISTS cart_detail (
     CONSTRAINT fk_cart_detail_cart FOREIGN KEY (cart_id) REFERENCES carts(id) ON DELETE CASCADE,
     CONSTRAINT fk_cart_detail_pro_config FOREIGN KEY (pro_configuration_id) REFERENCES pro_configuration(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE wishlist (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    product_id BIGINT NOT NULL,
+    created_date DATETIME DEFAULT NOW(),
+    UNIQUE KEY unique_user_product (user_id, product_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
+INSERT INTO roles (`name`, `description`) VALUES
+('USER', 'Người dùng bình thường'),
+('ADMIN', 'Quản trị hệ thống'),
+('STAFF', 'Nhân viên');
+
+-- User bình thường
+INSERT INTO users (username, email, `password`, full_name, role_id) VALUES
+('user', 'user@gmail.com', '$2a$10$yWabUhcc84uStXPyJa.4j.4vq.uH53IwL5ybQc2VJNhuHx9tk9faG', 'User Example', 1);
+
+-- Admin
+INSERT INTO users (username, email, `password`, full_name, role_id) VALUES
+('admin', 'admin@gmail.com', '$2a$10$yWabUhcc84uStXPyJa.4j.4vq.uH53IwL5ybQc2VJNhuHx9tk9faG', 'Admin Example', 2);
+
+-- Staff
+INSERT INTO users (username, email, `password`, full_name, role_id) VALUES
+('staff', 'staff@gmail.com', '$2a$10$yWabUhcc84uStXPyJa.4j.4vq.uH53IwL5ybQc2VJNhuHx9tk9faG', 'Staff Example', 3);
