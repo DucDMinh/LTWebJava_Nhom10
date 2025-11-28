d539bf5fcc8fe18be0ceaa85f0c5361a33cb345a
DucM <119028571+DucDMinh@users.noreply.github.com>
2025-11-28 17:15:15 +0700

    Implement SpringBootServletInitializer in Nhom10Application
    Extend Nhom10Application to support servlet initialization.

--- nhom10/src/main/java/com/haui/Nhom10Application.java
+++ nhom10/src/main/java/com/haui/Nhom10Application.java
@@ -2,12 +2,16 @@ package com.haui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder; // 1. Import dòng này
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // 2. Import dòng này
 @SpringBootApplication
-public class Nhom10Application {
+public class Nhom10Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Nhom10Application.class, args);
 	}
-
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Nhom10Application.class);
+    }
 }
